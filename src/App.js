b1ea1279cd5ab453a65a9de431153a3e903f8a6f
mgusmano <mgusmano@yahoo.com>
2021-07-16 14:02:49 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { API, graphqlOperation } from 'aws-amplify'
+import { listOperators as ListOperators } from './graphql/queries'
 
 function App() {
+  const [operators, setOperators] = useState([])
+
+  useEffect(() => {
+    getData()
+
+  },[])
+
+  async function getData() {
+    const operatorData = await API.graphql(graphqlOperation(ListOperators))
+    setOperators(operatorData.data.listOperators.items)
+  }
+
+
+
+
   return (
     <div style={{ textAlign: 'center' }}>
       <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+      {
+        operators.map((item, index) => (
+          <div key={index}>
+
+            <h5>{item.name}</h5>
+
+          </div>
+        ))
+      }
       </header>
     </div>
   );
