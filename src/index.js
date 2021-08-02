5c263da66bdccee0be2945804216606192f946e9
mgusmano <mgusmano@yahoo.com>
2021-08-02 17:19:35 -0400

    hashrouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+//import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 
 import Amplify from 'aws-amplify'
