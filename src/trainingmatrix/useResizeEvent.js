2f311cf90828f8ac5345d103de65c4c287c3a322
mgusmano <mgusmano@yahoo.com>
2021-11-16 13:05:33 -0500

    resize

--- src/trainingmatrix/useResizeEvent.js
+++ src/trainingmatrix/useResizeEvent.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useMatrixState } from './state/MatrixProvider';
 
 export function useResizeEvent() {
-  //console.log('useResizeEvent',window.innerWidth)
+  console.log('useResizeEvent',window.innerWidth)
   const matrixState = useMatrixState();
 
   var event = 'resize'
@@ -11,20 +11,29 @@ export function useResizeEvent() {
   const handler = () => {
     var multiplier = matrixState.original.multiplier
     var col1 = 0, row2 = 0, topHeight = 0, row2Orig = 0;
-    if (window.innerWidth <1000) {
+    if (window.innerWidth <10) {
+      //multiplier = 5;
       col1 = 0*multiplier;
       topHeight = 5;
       //row2 = ((matrixState.original.row2*2)*multiplier)+(topHeight*multiplier);
       row2Orig = matrixState.original.row2*multiplier;
       row2 = ((matrixState.original.row2*2)*multiplier);
     }
+    else if (window.innerWidth <1500) {
+      //multiplier = 6;
+      col1 = matrixState.original.col1*multiplier;
+      row2Orig = matrixState.original.row2*multiplier;
+      row2 = matrixState.original.row2*multiplier;
+    }
     else {
+      //multiplier = 7;
       col1 = matrixState.original.col1*multiplier;
       row2Orig = matrixState.original.row2*multiplier;
       row2 = matrixState.original.row2*multiplier;
     }
     //console.log(row2)
     var d ={
+      multiplier: multiplier,
       topHeight: topHeight*multiplier,
       fontsize: matrixState.original.fontsize*multiplier,
       bandX: matrixState.original.bandX*multiplier,
